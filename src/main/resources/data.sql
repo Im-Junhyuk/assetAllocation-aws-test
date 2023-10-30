f176b55633e43bfc4d6e69dd129de25de9435a0c
Im-Jumhyuk <junhyuk1027@gmail.com>
2023-10-30 18:58:24 +0900

    ..

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
--- insert into "MEMBER" (MEMBER_ID, LOGIN_ID, PASSWORD) values (1, 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi');
--- insert into "MEMBER" (MEMBER_ID, LOGIN_ID, PASSWORD) values (2, 'user', '$2a$10$bEbKOAFO1zD3iXzcVnZF1.qco0BkZY95CYnnYn/RhR6yYINgPzyWG');
---
--- -- insert into authority (authority_name) values ('ROLE_USER');
--- -- insert into authority (authority_name) values ('ROLE_ADMIN');
---
--- -- insert into MEMBER_AUTHORITY (MEMBER_ID, AUTHORITY_NAME) values (1, 'ROLE_USER');
--- insert into MEMBER_AUTHORITY (MEMBER_ID, AUTHORITY_NAME) values (1, 'ROLE_ADMIN');
--- insert into MEMBER_AUTHORITY (MEMBER_ID, AUTHORITY_NAME) values (2, 'ROLE_USER');
