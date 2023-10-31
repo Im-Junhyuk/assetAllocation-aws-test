5273a07eb3eb08be7c0c79df98e1ad35ce155a79
Im-Jumhyuk <junhyuk1027@gmail.com>
2023-10-31 16:51:43 +0900

    backtest controller for test added

--- src/main/java/graduationproject/assetallocation/controller/BacktestController.java
+++ src/main/java/graduationproject/assetallocation/controller/BacktestController.java
@@ -1,9 +1,13 @@
 package graduationproject.assetallocation.controller;
 
 import graduationproject.assetallocation.domain.aa.Aa;
+import graduationproject.assetallocation.domain.dto.DaaDTO;
+import graduationproject.assetallocation.domain.dto.SaaDTO;
 import graduationproject.assetallocation.service.AaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +15,18 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/bt")
 public class BacktestController {
 
     private final AaService aaService;
-    public String SAABT(Long aAId){
-        Optional<Aa> aA = aaService.findById(aAId);
-        // 장고 호출
+
+    @PostMapping("/backtest_static")
+    public String SaaBacktest(@RequestBody SaaDTO saaDTO){
+
+        return "ok";
+    }
+
+    @PostMapping("/backtest_dynamic")
+    public String DaaBacktest(@RequestBody DaaDTO daaDTO){
 
         return "ok";
     }
