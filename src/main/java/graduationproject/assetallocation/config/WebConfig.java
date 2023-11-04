cb2a17b819cffc5ffedc60db57e1089c39b67fa1
Im-Jumhyuk <junhyuk1027@gmail.com>
2023-11-04 14:43:33 +0900

    cors ip address added

--- src/main/java/graduationproject/assetallocation/config/WebConfig.java
+++ src/main/java/graduationproject/assetallocation/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.0.15:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "Options")
+                .allowedOrigins("http://192.168.0.15:3000", "http://210.221.36.82:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
